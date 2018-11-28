d68661c132606fd58939537246283f8e45f1f46c
Hack Reactor Student <student@hackreactor.com>
2018-11-28 15:17:04 -0800

    Completed currentVideo and videoList reducers.

--- src/reducers/videoList.js
+++ src/reducers/videoList.js
@@ -1,7 +1,11 @@
 import Redux from 'redux';
 
-var videoListReducer = (state = sampleData, action) => {
-  //TODO: define a reducer for the videoList field of our state.
+var videoListReducer = (state = [], action) => {
+  if (action.type = 'CHANGE_VIDEO_LIST' && action.videos){
+    return action.videos;
+  } else {
+    return state;
+  }
 };
 
 export default videoListReducer;
