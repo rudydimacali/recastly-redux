de5a37d3027b94da9ccc7d530d6733a8e5091847
Hack Reactor Student <student@hackreactor.com>
2018-11-28 15:15:55 -0800

    Completed currentVideo, search, and videoList actions.

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,8 +5,8 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
-  //TODO:  Write an asynchronous action to handle a video search!
+  type: 'HANDLE_VIDEO_SEARCH';
+  query: q;
 };
 
 export default handleVideoSearch;
